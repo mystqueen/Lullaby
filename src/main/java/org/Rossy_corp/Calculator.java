6f79ff8ffb69e4c9b5d13a31d413476237138c60
User <queenefya669@gmail.com>
2023-11-13 16:06:43 +0000

    feat: add addition and subtraction methods to calculator class

--- src/main/java/org/Rossy_corp/Calculator.java
+++ src/main/java/org/Rossy_corp/Calculator.java
@@ -2,4 +2,15 @@ package org.Rossy_corp;
 
 public class Calculator {
   public Calculator() {}
+
+  public double addition(double firstNumber, double secondNumber) {
+    double sum = firstNumber + secondNumber;
+    return sum;
+  }
+
+  public double subtraction(double firstNumber, double secondNumber){
+    double difference = firstNumber - secondNumber;
+    return difference;
+  }
+
 }
